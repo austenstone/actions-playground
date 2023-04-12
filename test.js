0e98f35866ea1277bd94e047b4b177cb86f92e92
Austen Stone <austenstone@github.com>
2023-04-12 11:42:03 -0400

    test

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const content = fs.readFileSync('test.json');
 const json = JSON.parse(content);
 const parts = json.monitoredResourceId.split('/');
-parts[1] = '987654321';
+parts[2] = '987654321';
 parts.join('/');
 json.monitoredResourceId = parts;
 fs.writeFileSync('test.json', JSON.stringify(json));
