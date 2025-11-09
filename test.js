2fd091ba9addd64c1a83181df52946d4368ee4a3
Austen Stone <austenstone@github.com>
2025-11-09 04:15:08 -0500

    Remove deprecated getting started guide and test files

--- test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('test.json');
-const json = JSON.parse(content);
-const parts = json.monitoredResourceId.split('/');
-parts[2] = '987654321';
-parts.join('/');
-json.monitoredResourceId = parts;
-fs.writeFileSync('test.json', JSON.stringify(json));
-return json.monitoredResourceId
